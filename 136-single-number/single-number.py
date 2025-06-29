5164dd9b84202d0873e480fee2ef553fb9462eab
Brandon Michaels <59707657+Brandon-Michaels@users.noreply.github.com>
2025-06-29 16:54:49 -0400

    Time: 0 ms (100.00%) | Memory: 19.6 MB (43.65%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,19 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # Naive Approach
-        # O(n) time and O(n) space
-        # visited = set()
-        # visitedTwice = set()
-
-        # for i in range(len(nums)):
-        #     if nums[i] in visited:
-        #         visitedTwice.add(nums[i])
-        #     visited.add(nums[i])
-        
-        # for i in range(len(nums)):
-        #     if nums[i] not in visitedTwice:
-        #         return nums[i]
-
         # Optimal Approach
         # Bit Manipulation
         # 0 XOR anything => anything, 0 ^ num = num
