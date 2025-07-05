ba945585bb610aab610ecca67dd1d458c9f23271
Brandon Michaels <59707657+Brandon-Michaels@users.noreply.github.com>
2025-07-05 11:36:11 -0400

    Time: 4 ms (48.65%) | Memory: 19 MB (18.13%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,15 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         # Approach:
-        # Time-Complexity: O(nlogn), n is len(nums)
+        # Math, i should be equal to nums[i]
+        # 0 == 0, 1 == 1, etc,.
+        # so if res starts at len(nums)
+        # i == nums[i], then i - nums[i] == 0
+        # Time-Complexity: O(n)
         # Space-Complexity: O(1)
-        nums.sort()
-        i = -1
-        for j in range(len(nums)):
-            print(nums[j])
-            print(i + 1)
-            if (i + 1) != nums[j]:
-                return i + 1
-            i += 1
-        
-        return len(nums)
+
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += (i - nums[i])
+
+        return res
